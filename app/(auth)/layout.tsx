a573301fe0e2b498d907e316346a0bac6d54effe
weru <weru.marsha@gmail.com>
2023-11-30 07:18:33 -0800

    new file for clerk auth API keys

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,27 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+
+import '../globals.css';
+
 export const metadata = {
     title: 'Mbogi',
     description: 'A Next.js 13 Meta Threads clone'
 }
+
+const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ 
     children 
 }: { 
     children: React.ReactNode
  }) {
-    return 
+    return (
+      <ClerkProvider> /*Wrap layout with clerkProvider to allow use all of clerks functionalities*/
+        <html lang="en">
+            <body className={`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+        </html>
+      </ClerkProvider>
+    )
 }
