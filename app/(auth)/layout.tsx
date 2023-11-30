b23fa3a2d739a904a314b78608045f2b647264ac
weru <weru.marsha@gmail.com>
2023-11-30 06:35:27 -0800

    Layouts with children in it because we'll display

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,3 +2,11 @@ export const metadata = {
     title: 'Mbogi',
     description: 'A Next.js 13 Meta Threads clone'
 }
+
+export default function RootLayout({ 
+    children 
+}: { 
+    children: React.ReactNode
+ }) {
+    return 
+}
